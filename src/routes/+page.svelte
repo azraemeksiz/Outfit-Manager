bb56e364d8e9c716039d1f6e120b31d46ddad966
Azra nur emeksiz <azranuremeksiz@gmail.com>
2026-03-05 19:03:58 +0100

    supabase connection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script lang="ts">
+  import { supabase } from '$lib/supabase'
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    const { data, error } = await supabase.from('profiles').select('*')
+    console.log('data:', data)
+    console.log('error:', error)
+  })
+</script>
+
+<h1>Test</h1> 
